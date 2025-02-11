a3d64ae97f43c35d495a53d5450f8642496f4de6
Luka <monster.striekr21@gmail.com>
2025-02-11 11:02:34 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// ხის კვანძის სტრუქტურა/ Binary tree structure
+struct Node {
+    string name;
+    string email;
+    Node* left;
+    Node* right;
+
+    // კონსტრუქტორი/ Constructor
+    Node (string n, string e) {
+        name = n;
+        email = e;
+        left = right = nullptr;
+
+    }
+};
